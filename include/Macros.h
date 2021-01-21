7a640d5bb39cd09887b1b644c02e47796c93e0cb
DvirPerkin <Dvirperkin@gmail.com>
2021-01-21 14:11:05 +0200

    Update Macros.h

--- include/Macros.h
+++ include/Macros.h
@@ -1,9 +1,17 @@
 #pragma once
 
-enum ScreenType_t {MAIN_MENU, STAGE, STAGE_CONSTRUCTION, EXIT};
-enum Sounds_t {COLLISION_ENEMY, COLLISION_COIN, LEVEL_FINISH, LOW_TIME};
-enum Textures_t {PLAYER, STUPID_ENEMY_TEXT, RAND_ENEMY_TEXT, SMART_ENEMY_TEXT, WALL, COIN, LADDER, POLE, GIFT, STAGE_DETAILS, STAGE_BACKGROUND, MENU_BACKGROUND};
+enum ScreenType_t {MAIN_MENU, STAGE, EXIT};
+
+enum Sounds_t {WIN, GAME_OVER, LEVELUP, ENEMY_COLLISION, SYRINGE_SOUND, LOW_TIME, OPEN_CURTAIN,
+               LIVE_GIFT_SOUND, SCORE_GIFT_SOUND, TIME_GIFT_SOUND, ENEMY_GIFT_SOUND};
+
+enum Music_t {MAIN_MENU_MUSIC, STAGE_MUSIC};
+
+enum Textures_t {PLAYER, STUPID_ENEMY_TEXT, RAND_ENEMY_TEXT, SMART_ENEMY_TEXT, WALL, COIN, LADDER,
+                 POLE, GIFT, STAGE_DETAILS, STAGE_BACKGROUND, MENU_BACKGROUND, WIN_SCREEN};
+
 enum EnemyType_t {STUPID_ENEMY, RAND_ENEMY, SMART_ENEMY};
+
 enum GiftType_t {LIVE_GIFT, SCORE_GIFT, TIME_GIFT, ENEMY_GIFT};
 
 const int WINDOW_HEIGHT = 1080;
@@ -12,11 +20,12 @@ const int MENU_BUTTON_HEIGHT = 100;
 const int GRAY = 100;
 const int MENU_BUTTON_WIDTH = WINDOW_WIDTH / 3;
 const int STAGE_DETAILS_SIZE = 50;
-const int NUM_OF_TEXTURES = 12;
-const int NUM_OF_SOUNDS = 4;
+const int NUM_OF_TEXTURES = 13;
+const int NUM_OF_SOUNDS = 11;
+const int NUM_OF_MUSICS = 2;
+const int NUM_BUTTONS = 2;
 const int THICKNESS = 7;
 const int NO_THICKNESS = 0;
-const int START_LEVEL = 1;
 const int MOD3 = 3;
 const int MOD4 = 4;
 const int LIVE = 3;
@@ -26,6 +35,7 @@ const int GIFT_SCORE = 100;
 const int ADD_TIME = 10;
 const int DO_NOTHING = -1;
 const int CHAR_SIZE = 50;
+const int MENU_BUTTON_CHAR_SIZE = 70;
 const int OFFSET_Y = 5;
 const int NUM_OF_DETAILS = 4;
 const int LIVE_DETAIL = 0;
@@ -34,10 +44,18 @@ const int LEVEL_DETAIL = 2;
 const int TIME_DETAIL = 3;
 const int MINUTE = 60;
 const int HOUR = 60;
+const int DEAD_WAITING_TIME = 2;
+const int FINISH_WAITING_TIME = 5;
+const int NO_TIME = -1;
+const int STAGE_SIZE_FACTOR = 60;
+const int VOLUME = 20;
+const int LEVELUP_WAITING_TIME = 2;
 
 const float TIME_ELAPSED_LIMIT = 0.05;
 const float WALL_DISPOSED_TIME = 5;
 const float MOVING_FACTOR = 0.8;
+const float SMALL_TIME_LIMIT = 0.4f;
+const float BIG_TIME_LIMIT = 1.4f;
 
 const char EMPTY = ' ';
 const char PLAYER_SYMBOL = '@';
@@ -48,13 +66,12 @@ const char LADDER_SYMBOL = 'H';
 const char POLE_SYMBOL = '-';
 const char GIFT_SYMBOL = '+';
 
-
 const int ENEMY_UP = 0;
 const int ENEMY_DOWN = 1;
 const int ENEMY_LEFT = 2;
 const int ENEMY_RIGHT = 3;
 
-const auto SPEED_PER_SECOND = 300.f;
+const auto SPEED_PER_SECOND = 200.f;
 const sf::Vector2f LEFT = {-1 , 0};
 const sf::Vector2f RIGHT = {1 , 0};
 const sf::Vector2f UP = {0 , -1};
@@ -68,6 +85,8 @@ const sf::Vector2f REFLECTION_DOWN = {1 , 1};
 //Smart Enemy
 enum graphStaticObjects_t {LADDER_OBJECT , POLE_OBJECT , NULL_OBJECT , WALL_OBJECT};
 
+const int NO_PREDECESSOR = -1;
+const int NO_NEIGHBOR = -1;
 const int NEIGHBOR_UP = 0;
 const int NEIGHBOR_DOWN = 1;
 const int NEIGHBOR_RIGHT = 2;
