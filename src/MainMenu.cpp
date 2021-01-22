08fee408c9b226aedb831b26e5263b138bcf0d0c
DvirPerkin <Dvirperkin@gmail.com>
2021-01-22 17:21:23 +0200

    Main Menu Update.
    Stage Construction button was deleted.
    The background music of the main menu is activated from the Sound class.

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -1,9 +1,42 @@
 #include "MainMenu.h"
 
-MainMenu::MainMenu() : m_lastRectangleBold(-1){
+MainMenu::MainMenu() :  m_lastRectangleBold(DO_NOTHING),
+                        m_title("Lode Runner",Font::FontObject().getFont()),
+                        m_subTitle("(Corona Virus Version)",Font::FontObject().getFont()),
+                        m_backGround(Textures::texturesObject().getSprite(MENU_BACKGROUND))
+{
+    //Initializes the background
+    m_backGround.scale(WINDOW_WIDTH / m_backGround.getGlobalBounds().width,
+                       WINDOW_HEIGHT / m_backGround.getGlobalBounds().height);
 
-   for(int index = 0; index < 3; index++)
-      m_menu.push_back(MenuButton(index));
+    //Initializes the title
+    m_title.setCharacterSize(CHAR_SIZE * 2);
+    m_title.setFillColor(sf::Color::White);
+    m_title.setPosition(WINDOW_WIDTH / 2 ,WINDOW_HEIGHT / m_title.getGlobalBounds().height * 5);
+    m_title.setOrigin(m_title.getGlobalBounds().width / 2, m_title.getGlobalBounds().height / 2);
+
+
+    //Initializes the sub - title.
+    m_subTitle.setCharacterSize(CHAR_SIZE);
+    m_subTitle.setFillColor(sf::Color::White);
+    m_subTitle.setOrigin(m_subTitle.getGlobalBounds().width / 2, m_subTitle.getGlobalBounds().height / 2);
+    m_subTitle.setPosition(WINDOW_WIDTH / 2 ,
+                           m_title.getGlobalBounds().top + m_title.getGlobalBounds().height + m_subTitle.getGlobalBounds().height);
+
+
+    //Initializes menu buttons.
+   for(int index = 0; index < NUM_BUTTONS; index++) {
+       switch(index){
+
+           case 0:
+               m_menuButton.push_back(MenuButton(index, "New Game"));
+               break;
+
+           case 1:
+               m_menuButton.push_back(MenuButton(index, "Exit"));
+               break;
+       }
+   }
 }
 //=============================================================================
 enum ScreenType_t MainMenu::display(sf::RenderWindow & window) {
@@ -12,7 +45,8 @@ enum ScreenType_t MainMenu::display(sf::RenderWindow & window) {
     draw(window);
     window.display();
 
-    //Music();
+    if(!Sound::soundObject().checkMusicPlay(MAIN_MENU_MUSIC))
+        Sound::soundObject().playMusic(MAIN_MENU_MUSIC);
 
     if (auto event = sf::Event{}; window.waitEvent(event))
         switch (event.type)
@@ -22,12 +56,10 @@ enum ScreenType_t MainMenu::display(sf::RenderWindow & window) {
                 break;
 
             case sf::Event::MouseButtonPressed:
-                if(event.mouseButton.button == sf::Mouse::Left)
-                {
-                    for(int index = 0; index < 3; index++)
-                    {
-                        if(m_menu[index].checkPressed(window.mapPixelToCoords({event.mouseButton.x , event.mouseButton.y}))) {
-                            m_backGroundMusic.stop();
+                if(event.mouseButton.button == sf::Mouse::Left){
+                    for(int index = 0; index < NUM_BUTTONS; index++){
+                        if(m_menuButton[index].checkPressed(window.mapPixelToCoords({event.mouseButton.x , event.mouseButton.y}))) {
+                            Sound::soundObject().stopMusic(MAIN_MENU_MUSIC);
                             return (enum ScreenType_t) (index + 1);
                         }
                     }
@@ -43,75 +75,41 @@ enum ScreenType_t MainMenu::display(sf::RenderWindow & window) {
 }
 //=============================================================================
 void MainMenu::draw(sf::RenderWindow & window) {
-    for(int index = 0; index < 3; index++)
-        m_menu[index].draw(window);
+
+    window.draw(m_backGround);
+    window.draw(m_title);
+    window.draw(m_subTitle);
+
+    for(int index = 0; index < NUM_BUTTONS; index++) {
+        m_menuButton[index].draw(window);
+    }
 }
 //=============================================================================
 void MainMenu::handleMouseMove(sf::RenderWindow &window, const sf::Vector2f &location) {
 
-    for (int index = 0; index < m_menu.size(); index++)
+    for (int index = 0; index < m_menuButton.size(); index++)
         {
-            if (m_menu[index].checkPressed(location))
+            if (m_menuButton[index].checkPressed(location))
             {
                 //Check if the mouse moved to another rectangle.
                 if (index != m_lastRectangleBold)
                 {
-                    m_menu[index].setOutlineThickness(THICKNESS);
+                    m_menuButton[index].setOutlineThickness(THICKNESS);
 
-                    if(m_lastRectangleBold != -1) {
-                        m_menu[m_lastRectangleBold].setOutlineThickness(0);
-                        m_menu[m_lastRectangleBold].draw(window);
+                    if(m_lastRectangleBold != DO_NOTHING) {
+                        m_menuButton[m_lastRectangleBold].setOutlineThickness(0);
+                        m_menuButton[m_lastRectangleBold].draw(window);
                     }
                     m_lastRectangleBold = index;
                 }
                 return;
             }
         }
-    if(m_lastRectangleBold != -1) {
-        m_menu[m_lastRectangleBold].setOutlineThickness(0);
+
+    if(m_lastRectangleBold != DO_NOTHING) {
+        m_menuButton[m_lastRectangleBold].setOutlineThickness(NO_THICKNESS);
     }
-    m_lastRectangleBold = -1;
+    m_lastRectangleBold = DO_NOTHING;
 
 }
 //=============================================================================
-void MainMenu::Music() {
-    if(m_backGroundMusic.getStatus() != m_backGroundMusic.Playing) {
-        if(!m_backGroundMusic.openFromFile("MainMenuMusic.ogg"))
-            exit(EXIT_FAILURE);
-
-        m_backGroundMusic.setLoop(true);
-        m_backGroundMusic.play();
-    }
-}
-//=============================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/MenuButton.cpp
+++ src/MenuButton.cpp
@@ -1,9 +1,19 @@
 #include "MenuButton.h"
 
-MenuButton::MenuButton(int position) : m_frame({MENU_BUTTON_WIDTH , MENU_BUTTON_HEIGHT}){
+MenuButton::MenuButton(int position, std::string text)
+    : m_frame({MENU_BUTTON_WIDTH , MENU_BUTTON_HEIGHT}), m_buttonText(text, Font::FontObject().getFont()){
 
     m_frame.setPosition(MENU_BUTTON_WIDTH, ((WINDOW_HEIGHT / 3) + (MENU_BUTTON_HEIGHT * position) + (50 * position)));
     m_frame.setOutlineColor({GRAY, GRAY, GRAY});
+    m_frame.setFillColor(sf::Color::Transparent);
+
+    m_buttonText.setCharacterSize(MENU_BUTTON_CHAR_SIZE);
+    m_buttonText.setOrigin(m_buttonText.getGlobalBounds().width / 2, m_buttonText.getGlobalBounds().height / 2);
+    m_buttonText.setPosition(getPosition().x + getGlobalBound().width / 2,
+                                    getPosition().y + getGlobalBound().height / 2 - CHAR_SIZE / 3);
+    m_buttonText.setFillColor(sf::Color::Black);
+    m_buttonText.setOutlineColor(sf::Color::White);
+    m_buttonText.setOutlineThickness(THICKNESS);
 }
 //=============================================================================
 bool MenuButton::checkPressed(const sf::Vector2f &location) {
@@ -16,9 +26,18 @@ bool MenuButton::checkPressed(const sf::Vector2f &location) {
 //=============================================================================
 void MenuButton::draw(sf::RenderWindow & window) {
     window.draw(m_frame);
+    window.draw(m_buttonText);
 }
 //=============================================================================
 void MenuButton::setOutlineThickness(int thickness) {
     m_frame.setOutlineThickness(thickness);
+}
+//=============================================================================
+sf::Vector2f MenuButton::getPosition() const{
+    return m_frame.getPosition();
+}
+//=============================================================================
+sf::FloatRect MenuButton::getGlobalBound() const {
+    return m_frame.getGlobalBounds();
 }
 //=============================================================================
